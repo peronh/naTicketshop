2a08f58e5a7ab3d73d167dc62f31324336534886
peronh <pronyecz@gmail.com>
2018-06-10 20:49:28 +0200

    update html, css & main.js flies

--- js/main.js
+++ js/main.js
@@ -63,10 +63,154 @@ $(".cherry-custom-file").on("change", function (ev) {
       .find(".file-name")
       .html(name);
   });
-  
+
+  var alertBox = $(".alert.alert-primary");
+
+  function showInvalidMessage() {
+    alertBox
+      .removeClass("alert-primary")
+      .addClass(".alert-danger")
+      .find(".alert-message")
+      .text("sikertelen belépés");
+  }
+
  // ticket oldal
   
   $(function () {
     $('[data-toggle="tooltip"]').tooltip();
   });
-  
+
+  // jegyek tömbje.
+
+  var tickets = [
+    {
+      event: "Sziget fesztivál",
+      time: "2018-08-03 18:00:00",
+      seller: "Kiss Márton",
+      pcs: "6",
+      link: "licit/1"
+    },
+    {
+      event: "Diótörő balett",
+      time: "2018-08-03 18:00:00",
+      seller: "Nagy Attila",
+      pcs: "15",
+      link: "licit/1"
+    },
+    {
+      event: "MOMA Party",
+      time: "2018-08-03 18:00:00",
+      seller: "Czakó János",
+      pcs: "6",
+      link: "licit/1"
+    },
+    {
+      event: "A Kékszakálú herceg vára",
+      time: "2018-08-03 18:00:00",
+      seller: "Márton Zita",
+      pcs: "9",
+      link: "licit/1"
+    },
+    {
+      event: "Balett mindenkinek",
+      time: "2018-08-03 18:00:00",
+      seller: "Takács Bernadett",
+      pcs: "13",
+      link: "licit/1"
+    },
+    {
+      event: "Macskák musical",
+      time: "2018-08-03 18:00:00",
+      seller: "Kiss Márton",
+      pcs: "32",
+      link: "licit/1"
+    }
+  ];
+
+  // jegyek táblájának generálása
+
+  var ticketTable = $("table.table.table-striped").eq(0);
+
+  function fillTicketstable(currentTickets) {
+
+    currentTickets = currentTickets || tickets ;
+
+    var tbody = ticketTable.find("tbody");
+    tbody.html("");
+    $.each( currentTickets, function(index, ticket) {
+      var row = $(".templates .ticket-row").clone();
+      row.find("td").eq(0).html(index+1);
+      row.find("td").eq(1).html(ticket.event);
+      row.find("td").eq(2).html(ticket.time);
+      row.find("td").eq(3).html(ticket.seller);
+      row.find("td").eq(4).html(ticket.pcs);
+      row.find("td").eq(5).html(ticket.link);
+
+      tbody.append(row);
+  });
+}
+fillTicketstable();
+
+// jegyek táblájának generálása
+
+$(".tickets-search-row input").on("keyup", filterTickets);
+
+function filterTickets() {
+ var currentValue = $(this).val().toLowerCase();
+ var filteredTickets = [];
+ if ( currentValue == "" ) {
+   filteredTickets = tickets;
+  }
+  else {
+    filteredTickets = tickets.filter( function(item) {
+      var done = false;
+    for ( var k in item ) {
+      if ( item [k].toString().toLowerCase().indexOf(currentValue) > -1 ) {
+        done = true;
+      }
+    }
+    return done;
+    });
+  }
+  fillTicketstable(filteredTickets);
+}
+
+// jegyek táblázat rendezése
+
+ticketTable.find("thead th[data-key]").on("click", orderTicketTable);
+
+  function orderTicketTable() {
+
+    var th = $(this);
+    $.each(ticketTable.find("thead th[data-key]"), function(index, elem){
+        var currentTh = $(elem);
+        if (th.data("key") != currentTh.data("key")){
+          currentTh.removeClass("asc").removeClass("desc")
+        }
+    });
+    var key = th.data("key");
+    var sortedTickets = tickets.map( function(item) {
+
+      return item;
+    });
+
+    if (th.hasClass("asc")){
+      th.removeClass("asc").addClass("desc");
+    }
+    else {
+      th.removeClass("desc").addClass("asc");
+    }
+   
+
+    sortedTickets.sort( function (a,b){
+      if (th.hasClass("asc")) {
+        return a[key].toString().localeCompare(b[key].toString());
+      }
+      else{
+        return b[key].toString().localeCompare(a[key].toString());
+      }
+      
+    });
+    fillTicketstable(sortedTickets);
+
+  }
